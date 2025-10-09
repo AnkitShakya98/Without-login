89d10cd95bc638f3c4b33383e395344d4f058224
AnkitShakya98 <vimla3119@gmail.com>
2025-10-09 20:22:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1654,6 +1654,6 @@ async def process_appxwp(bot: Client, m: Message, user_id: int):
                 await session.close()
             await CONNECTOR.close()
 
-bot.run()
+app.run()
 if __name__ == "__main__":
     asyncio.run(main())                                        
